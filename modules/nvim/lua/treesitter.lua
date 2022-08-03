4cd8384e3233b15af5331fc0e5e6dc707c0b41f8
eprader <56026248+eprader@users.noreply.github.com>
2022-08-03 23:01:17 +0200

    add bash

--- modules/nvim/lua/treesitter.lua
+++ modules/nvim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "nix", "lua", "c", "java", "haskell" },
+  ensure_installed = { "nix", "lua", "bash", "c", "java", "haskell" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
