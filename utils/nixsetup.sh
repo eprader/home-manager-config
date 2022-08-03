ef21ea7f9c11710667d23006b602514355691f3c
eprader <56026248+eprader@users.noreply.github.com>
2022-08-03 10:49:22 +0200

    changes to utils

--- utils/nixsetup.sh
+++ utils/nixsetup.sh
@@ -1,12 +1,12 @@
 #prerequisites for this script are: curl being installed
 
-#installing nix
+#install nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon && source $HOME/.nix-profile/etc/profile.d/nix.sh
 
 #enable nix flakes
 #nix-env -iA nixpkgs.nixFlakes
 
-#installing home-manager
+#install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager &&
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable &&
 nix-channel --update &&
@@ -15,14 +15,13 @@ nix-shell '<home-manager>' -A install &&
 nix-channel --add https://github.com/guibou/nixGL/archive/main.tar.gz nixgl && nix-channel --update &&
 nix-env -iA nixgl.auto.nixGLDefault && 
 
-#removing old .bashrc and .profile
+#remove default files
+rm ~/.config/nixpkgs/home.nix ~/.bashrc ~/.profile &&
 
 #symlinking home.nix
 rm ~/.config/nixpkgs/home.nix &&
 ln -s ~/home-manager-config/home.nix ~/.config/nixpkgs/home.nix &&
 
-#removing old files
-rm ~/.bashrc ~/.profile &&
 
-#running home-manager
+#switch to home-manager config
 home-manager switch
