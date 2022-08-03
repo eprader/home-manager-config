c597f2c05f17b37c06fd596350d8f4c49e14cd51
eprader <56026248+eprader@users.noreply.github.com>
2022-08-03 22:03:12 +0200

    going from alacritty to kitty

--- home.nix
+++ home.nix
@@ -11,6 +11,7 @@ in
     stateVersion = "22.05";
 
     sessionVariables = {
+      TERMINAL = "nixGL kitty";
       EDITOR = "nvim";
       VISUAL = "$EDITOR";
     };
@@ -117,21 +118,56 @@ in
     };
   };
 
-  /*  programs.alacritty = {
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_size = "11.0";
+      font_family = "SourceCodePro Nerd Font Mono";
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+      background = "#282828";
+      foreground = "#ebdbb2";
+      selection_foreground = "#93a1a1";
+      selection_background = "#002b36";
+      cursor = "#a89984";
+
+      color0 = "#282828";
+      color1 = "#cc241d";
+      color2 = "#98971a";
+      color3 = "#fabd2f";
+      color4 = "#458588";
+      color5 = "#b16286";
+      color6 = "#8ec07c";
+      color7 = "#a89984";
+
+      color8 = "#928374";
+      color9 = "#fb4934";
+      color10 = "#b8bb26";
+      color11 = "#fabd2f";
+      color12 = "#83a598";
+      color13 = "#d3869b";
+      color14 = "#8ec07c";
+      color15 = "#ebdbb2";
+
+    };
+  };
+
+  /*programs.alacritty = {
     enable = true;
 
     settings = {
     window = {
     title = "Terminal";
     padding = {
-    x = 2;
-    y = 2;
+    x = 1;
+    y = 1;
     };
-    opacity = 1.0;
+    opacity = 1;
     };
 
     font = {
-    normal.family = "SourceCodePro Nerd Font";
+    #normal.family = "SourceCodePro";
     size = 11.0;
     };
 
@@ -167,9 +203,9 @@ in
     };
     };
 
-    shell.program = "${pkgs.bash}/bin/bash";
+    # shell.program = "${pkgs.bash}/bin/bash";
 
     };
-    }; */
+    };*/
 
 }
