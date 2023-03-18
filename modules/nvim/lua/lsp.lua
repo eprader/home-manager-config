95e7fc2e52262814df13e8d37a75abbc88bda401
eprader <56026248+eprader@users.noreply.github.com>
2023-03-18 15:35:25 +0100

    changes due to deprecation

--- modules/nvim/lua/lsp.lua
+++ modules/nvim/lua/lsp.lua
@@ -55,7 +55,7 @@ require 'lspconfig'.ccls.setup {
   lsp = {codelens = {enable = true}}
 }
 
-require 'lspconfig'.sumneko_lua.setup {
+require 'lspconfig'.lua_ls.setup {
   on_attach = on_attach,
   flags = lsp_flags,
   capabilities = capabilities,
