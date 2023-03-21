b046e13a3be46af46752128750cbdf8bf4ef9a3c
eprader <56026248+eprader@users.noreply.github.com>
2023-03-21 09:22:07 +0100

    add keybinds vimtex

--- modules/nvim/lua/vimtex.lua
+++ modules/nvim/lua/vimtex.lua
@@ -1,2 +1,7 @@
 vim.g.vimtex_view_general_viewer = 'SumatraPDF'
 vim.g.vimtex_compiler_method = 'tectonic'
+
+local map = vim.keymap.set
+map('n', '<leader>ll', ":VimtexCompile<cr>")
+map('n', '<leader>lv', ":VimtexView<cr>")
+map('n', '<leader>lt', ":VimtexTocToggle<cr>")
