8a7e77551e2e0898cfa5dbe92636d796e8fe5d47
eprader <56026248+eprader@users.noreply.github.com>
2023-10-06 20:27:00 +0200

    fix zathura

--- modules/nvim/lua/vimtex.lua
+++ modules/nvim/lua/vimtex.lua
@@ -1,5 +1,5 @@
 -- in WSl: vim.g.vimtex_view_general_viewer = '/mnt/c/Users/emanu/AppData/Local/SumatraPDF/SumatraPDF.exe'
-vim.g.vimtex_view_general_viewer = 'yathura'
+vim.g.vimtex_view_general_viewer = 'zathura'
 
 vim.g.vimtex_compiler_method = 'tectonic'
 
