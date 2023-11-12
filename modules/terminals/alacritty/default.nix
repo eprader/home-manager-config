af7d10a9f804fb0c6fdc7735e91d16e599247a98
eprader <56026248+eprader@users.noreply.github.com>
2023-11-12 21:36:27 +0100

    move terminals to modules

---
+++ modules/terminals/alacritty/default.nix
@@ -0,0 +1,74 @@
+{ ... }:
+{
+
+  home.packages = with pkgs; [
+    (nerdfonts.override {
+      fonts = [ "FiraCode" ];
+    })
+  ];
+
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      window = {
+        startup_mode = "Maximized";
+        title = "Terminal";
+        decorations = "full";
+        dynamic_padding = true;
+        dimensions = {
+          columns = 90;
+          lines = 30;
+        };
+        padding = {
+          x = 1;
+          y = 1;
+        };
+        opacity = 1;
+      };
+
+      font = {
+        normal.family = "FiraCode Nerd Font Mono";
+        size = 12;
+        bold = { style = "Bold"; };
+      };
+
+
+      colors = {
+        primary = {
+          background = "0x282828";
+          foreground = "0xebdbb2";
+        };
+        cursor = {
+          text = "0x282828";
+          cursor = "0xa89984";
+        };
+        normal = {
+          black = "0x282828";
+          red = "0xcc241d";
+          green = "0x98971a";
+          yellow = "0xd79921";
+          blue = "0x458588";
+          magenta = "0xb16286";
+          cyan = "0x689d6a";
+          white =
+            "0xa89984";
+        };
+        bright = {
+          black = "0x928374";
+          red = "0xfb4934";
+          green = "0xb8bb26";
+          yellow = "0xfabd2f";
+          blue = "0x83a598";
+          magenta = "0xd3869b";
+          cyan = "0x8ec07c";
+          white =
+            "0xebdbb2";
+        };
+      };
+
+      # shell.program = "${pkgs.bash}/bin/bash";
+
+    };
+  };
+}
---
+++ modules/terminals/kitty/default.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    (nerdfonts.override {
+      fonts = [ "FiraCode" ];
+    })
+  ];
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_size = "14.0";
+      font_family = "FiraCode Nerd Font Mono";
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+      background = "#282828";
+      foreground = "#ebdbb2";
+      selection_foreground = "#93a1a1";
+      selection_background = "#002b36";
+      cursor = "#a89984";
+
+      color0 = "#282828";
+      color1 = "#cc241d";
+      color2 = "#98971a";
+      color3 = "#fabd2f";
+      color4 = "#458588";
+      color5 = "#b16286";
+      color6 = "#8ec07c";
+      color7 = "#a89984";
+
+      color8 = "#928374";
+      color9 = "#fb4934";
+      color10 = "#b8bb26";
+      color11 = "#fabd2f";
+      color12 = "#83a598";
+      color13 = "#d3869b";
+      color14 = "#8ec07c";
+      color15 = "#ebdbb2";
+    };
+  };
+}
