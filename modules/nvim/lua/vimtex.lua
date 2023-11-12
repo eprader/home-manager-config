3daa26a760a788abe20be4b61c638ba6963449cf
eprader <56026248+eprader@users.noreply.github.com>
2023-11-12 21:38:33 +0100

    enable shellescape for minted to work

--- modules/nvim/lua/vimtex.lua
+++ modules/nvim/lua/vimtex.lua
@@ -1,6 +1,12 @@
 -- in WSl: vim.g.vimtex_view_general_viewer = '/mnt/c/Users/emanu/AppData/Local/SumatraPDF/SumatraPDF.exe'
 vim.g.vimtex_view_general_viewer = 'zathura'
-
+vim.g.vimtex_compiler_tectonic = {
+    options = {
+        '--keep-logs',
+        '--synctex',
+        '-Zshell-escape', -- NOTE: Needed to allow the minted package to call pygments
+    }
+}
 vim.g.vimtex_compiler_method = 'tectonic'
 
 local map = vim.keymap.set
