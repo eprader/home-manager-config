48b8cbbdaaab643de839ed42d256c39c7fd7b180
eprader <56026248+eprader@users.noreply.github.com>
2023-11-14 04:35:37 +0100

    try to add python packages to submodule

--- modules/latex/default.nix
+++ modules/latex/default.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
+let
+  # TODO: Python packages can not be merged that easily look into how to do so.
+  pythonPackages = p: with p; [
+    pygments
+  ];
+in
 {
   home.packages = with pkgs; [
     # LSP
