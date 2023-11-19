45f6e7033a88493f96d5ea38e9345c0a9ddaec16
eprader <56026248+eprader@users.noreply.github.com>
2023-11-19 02:56:02 +0100

    remove some windowops to fix rename probably rewrite in the future

--- modules/neovim/nvim/lua/eprader/dressing.lua
+++ modules/neovim/nvim/lua/eprader/dressing.lua
@@ -19,7 +19,7 @@ require('dressing').setup({
                 -- 'editor' and 'win' will default to being centered
                 relative = "cursor",
                 -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-                prefer_width = 40,
+                --[[ prefer_width = 40,
                 width = nil,
                 -- min_width and max_width can be a list of mixed types.
                 -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
@@ -29,7 +29,7 @@ require('dressing').setup({
                 win_options = {
                     winblend = 10,
                     winhighlight = "",
-                },
+                }, ]]
             }
 
             for key, value in pairs(conf_override) do
