0e0d14b14356af6cb9dffc3e083c8f4e4eb68bb8
eprader <56026248+eprader@users.noreply.github.com>
2023-11-19 03:59:08 +0100

    port some vim.cmd settings to vim. opt and rewrite on_yank autocommand

--- modules/neovim/nvim/lua/eprader/settings.lua
+++ modules/neovim/nvim/lua/eprader/settings.lua
@@ -1,5 +1,9 @@
+local vim_api = vim.api
+
 local settings = {
     hidden = true, -- opening new buffer without writing old one
+    swapfile = false,
+    showmode = false,
     errorbells = false,
     number = true,
     relativenumber = true,
@@ -29,12 +33,18 @@ for k, v in pairs(settings) do
     vim.opt[k] = v
 end
 
--- nvim-notify
-vim.notify = require 'eprader.notify'
-
-vim.api.nvim_command("set noswapfile")
-
+vim.cmd.colorscheme('gruvbox')
 vim.g.gruvbox_contrast_dark = 'medium'
+
+if (vim.g.colors_name == "gruvbox") then
+    vim_api.nvim_set_hl(0, '@parameter.python', { link = 'GruvboxYellowBold' })
+    vim_api.nvim_set_hl(0, 'Delimiter', { link = 'GruvboxGray' })
+end
+
+-- nvim-notify
+vim.notify = require 'notify' -- NOTE: Requires the plugin, not the personal `.lua` config
+
+-- TODO: check if system is WSL and apply condigional settings
 
 --[[ add to vim.cmd for clipboard in WSL
 let g:clipboard = {
@@ -49,21 +59,14 @@ let g:clipboard = {
 \   },
 \   'cache_enabled': 0,
 \ } ]]
-vim.cmd [[
-colorscheme gruvbox
-set noshowmode
-]]
 
-
-
-if (vim.g.colors_name == "gruvbox") then
-    vim.api.nvim_set_hl(0, '@parameter.python', { link = 'GruvboxYellowBold' })
-    vim.api.nvim_set_hl(0, 'Delimiter', { link = 'GruvboxGray' })
-end
-
-vim.cmd [[
-augroup highlight_yank
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=80})
-augroup END
-]]
+vim_api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank {
+            higroup = vim.fn["hlexists"]('HighlightedyankRegion') > 0
+                and 'HighlightedyankRegion'
+                or 'IncSearch',
+            timeout = 100
+        }
+    end
+})
