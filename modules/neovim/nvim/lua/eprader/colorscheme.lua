d382d9ef5cbcb6c7b52d6e23a6aae0f2e0c75bf1
eprader <56026248+eprader@users.noreply.github.com>
2023-11-19 17:58:43 +0100

    move colorscheme to separate module so it can be required earlier. Preparation for prequire gracefull require handling

---
+++ modules/neovim/nvim/lua/eprader/colorscheme.lua
@@ -0,0 +1,8 @@
+vim.cmd.colorscheme('gruvbox')
+vim.g.gruvbox_contrast_dark = 'medium'
+
+local api = vim.api
+if (vim.g.colors_name == "gruvbox") then
+    api.nvim_set_hl(0, '@parameter.python', { link = 'GruvboxYellowBold' })
+    api.nvim_set_hl(0, 'Delimiter', { link = 'GruvboxGray' })
+end
