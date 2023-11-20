7ba960451093ff9cb9d647cd2c10958fb904bdb7
eprader <56026248+eprader@users.noreply.github.com>
2023-11-20 15:57:01 +0100

    use prequire in init.lua

--- modules/neovim/nvim/lua/eprader/init.lua
+++ modules/neovim/nvim/lua/eprader/init.lua
@@ -1,25 +1,36 @@
-local prequire = require 'eprader.prequire'
-prequire 'eprader.colorscheme'
+local success, prequire = pcall(require, 'eprader.prequire')
+if not success then
+    vim.notify("Failed to load config: module `prequrie` Failed to load. Aborted loading configuration.")
+    return
+end
+
+-- NOTE: Load `notify` first to handle future `vim.notify` calls of `prequire` and others
 prequire 'eprader.notify'
--- INFO: Load `colorscheme` and `notify` first so that it applies to all following `prequire`.
+prequire 'failing'
+
 prequire 'eprader.settings'
 prequire 'eprader.keymaps'
-prequire 'eprader.lsp'
-prequire 'eprader.telescope'
+prequire 'eprader.colorscheme'
 prequire 'eprader.treesitter'
+prequire 'eprader.autopairs'
+prequire 'eprader.comment'
+
+prequire 'eprader.telescope'
 prequire 'eprader.lsp'
-prequire 'eprader.dressing'
 prequire 'eprader.cmp'
 prequire 'eprader.harpoon'
-prequire 'eprader.autopairs'
+
 prequire 'eprader.gitsigns'
-prequire 'eprader.dap'
-prequire 'eprader.dapui'
-prequire 'eprader.lualine'
-prequire 'eprader.nvim-r'
-prequire 'eprader.java'
+prequire 'eprader.dressing'
 prequire 'eprader.trouble'
 prequire 'eprader.todo-comments'
-prequire 'eprader.comment'
+prequire 'eprader.lualine'
+
 prequire 'eprader.toggleterm'
 prequire 'eprader.overseer'
+
+prequire 'eprader.dap'
+prequire 'eprader.dapui'
+
+prequire 'eprader.nvim-r'
+prequire 'eprader.java'
