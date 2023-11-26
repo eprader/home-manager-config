ecdbc3579032a058823ed6640e8479a7126c4fd5
eprader <56026248+eprader@users.noreply.github.com>
2023-11-26 01:29:34 +0100

    update ui for flameshot

--- home.nix
+++ home.nix
@@ -147,7 +147,7 @@ in
     enable = true;
     settings = {
       General = {
-        buttons = "@Variant(\\0\\0\\0\\x7f\\0\\0\\0\\vQList<int>\\0\\0\\0\\0\\x6\\0\\0\\0\\t\\0\\0\\0\\x10\\0\\0\\0\\n\\0\\0\\0\\v\\0\\0\\0\\x17\\0\\0\\0\\f)";
+        buttons = "@Variant(\\0\\0\\0\\x7f\\0\\0\\0\\vQList<int>\\0\\0\\0\\0\\x4\\0\\0\\0\\x2\\0\\0\\0\\x3\\0\\0\\0\\x5\\0\\0\\0\\x6)";
         contrastOpacity = 188;
         contrastUiColor = "#282828";
         showSidePanelButton = true;
