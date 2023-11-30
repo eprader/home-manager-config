421bd7c80185d3ae06b0c5373d955c1954919b6e
eprader <56026248+eprader@users.noreply.github.com>
2023-11-30 01:40:44 +0100

    rename prequire fix quotes

--- modules/nvim/lua/eprader/appearance/colorscheme.lua
+++ modules/nvim/lua/eprader/appearance/colorscheme.lua
@@ -1,15 +1,14 @@
-local prequire = require "eprader.prequire"
---local gruvbox = prequire "gruvbox_community"
+local require = require "eprader.prequire"
 
-vim.cmd.colorscheme('gruvbox')
-vim.g.gruvbox_contrast_dark = 'medium'
+vim.cmd.colorscheme("gruvbox")
+vim.g.gruvbox_contrast_dark = "medium"
 
 local api = vim.api
 if (vim.g.colors_name == "gruvbox") then
-    local treesitter = prequire "nvim-treesitter"
+    local treesitter = require "nvim-treesitter"
     if treesitter then
-        api.nvim_set_hl(0, '@parameter.python', { link = 'GruvboxYellowBold' })
+        api.nvim_set_hl(0, "@parameter.python", { link = "GruvboxYellowBold" })
     end
 
-    api.nvim_set_hl(0, 'Delimiter', { link = 'GruvboxGray' })
+    api.nvim_set_hl(0, "Delimiter", { link = "GruvboxGray" })
 end
