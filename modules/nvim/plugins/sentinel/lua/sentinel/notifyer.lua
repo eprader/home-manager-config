64b97c6187c4d5605120251ea2638f887c89d045
eprader <56026248+eprader@users.noreply.github.com>
2023-12-03 02:38:52 +0100

    fix bug in path truncation notification, implement debug_depth fallback and detect bug

--- modules/nvim/plugins/sentinel/lua/sentinel/notifyer.lua
+++ modules/nvim/plugins/sentinel/lua/sentinel/notifyer.lua
@@ -18,6 +18,7 @@ end
 
 function M._render_src_path(full_path, predecessor_depth, with_icon)
     local split_path = vim.split(full_path, "/")
+    -- print("split")
 
     if #split_path < predecessor_depth then
         local message =
@@ -26,7 +27,7 @@ function M._render_src_path(full_path, predecessor_depth, with_icon)
             .. "  - `predecessor_depth`: " .. predecessor_depth .. "\n"
             .. "\n"
             .. "Falling back to `available_depth`..."
-        M._notify(message, "info")
+        vim.schedule(function() vim.notify(message, "info", notify_options) end)
     end
 
     local path = table.concat(split_path, "/", math.max(1, #split_path - predecessor_depth), #split_path)
@@ -37,16 +38,28 @@ function M._render_src_path(full_path, predecessor_depth, with_icon)
 end
 
 function M._build_error_message(module_name, module_error)
-    local info = debug.getinfo(4, "Sl")
+    local debug_info = M._get_debug_info(5, "Sl")
     local module = (M.with_icons and "󰆦" or "module") .. ": __" .. module_name .. "__"
-    local path = M._render_src_path(info.source, M.pred_depth, M.with_icons)
+    local path = M._render_src_path(debug_info.source, M.pred_depth, M.with_icons)
 
     return
-        path .. " on line " .. info.currentline .. ".\n"
+        path .. " on line " .. debug_info.currentline .. ".\n"
         .. module .. ".\n"
         .. "\n"
         .. "```vim\n"
         .. module_error
         .. "\n```"
 end
+
+-- BUG: some function stack levels lead to strange debug_info.
+-- They appear as path = [C] and currentline = -1
+-- For an error in `.../eprader/init.lua` it can be reproduced with f = 6
+function M._get_debug_info(f, what)
+    local debug_info = debug.getinfo(f, what)
+    if debug_info then
+        return debug_info
+    end
+    return M._get_debug_info(f - 1, what)
+end
+
 return M
