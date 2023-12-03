87d3886bed8c17e052e0df4f5669cd76c3739cb2
eprader <56026248+eprader@users.noreply.github.com>
2023-12-03 02:37:44 +0100

    add pcall for  rename methods to start() and stop()

--- modules/nvim/plugins/sentinel/lua/sentinel/init.lua
+++ modules/nvim/plugins/sentinel/lua/sentinel/init.lua
@@ -5,19 +5,29 @@ M._real_require = _G.require
 function M._proxy_require(modname)
     local success, module = pcall(M._real_require, modname)
     if not success then
-        local notifyer = M._real_require "sentinel.notifyer"
+        local ok, notifyer = pcall(M._real_require, "sentinel.notifyer")
+        if not ok then
+            vim.schedule(function()
+                vim.notify(
+                    "Unable to load `sentinel.notifyer`",
+                    "error",
+                    { title = "Sentinel internal" }
+                )
+            end)
+            return
+        end
         notifyer.notify(modname, module)
     end
     return module
 end
 
-function M.set_proxy_require()
+function M.start()
     _G.require = M._proxy_require
 end
 
-function M.remove_proxy_require()
+function M.stop()
     _G.require = M._real_require
 end
 
-M.set_proxy_require()
+M.start()
 return M
