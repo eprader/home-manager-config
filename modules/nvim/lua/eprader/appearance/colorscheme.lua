3b07cdda2582a6ff6795bb38922ea11ce6a5dd88
eprader <56026248+eprader@users.noreply.github.com>
2023-12-10 06:59:32 +0100

    remove overrides due to updated colorscheme

--- modules/nvim/lua/eprader/appearance/colorscheme.lua
+++ modules/nvim/lua/eprader/appearance/colorscheme.lua
@@ -1,12 +1,2 @@
 vim.cmd.colorscheme("gruvbox")
 vim.g.gruvbox_contrast_dark = "medium"
-
-local api = vim.api
-if (vim.g.colors_name == "gruvbox") then
-    local treesitter = require "nvim-treesitter"
-    if treesitter then
-        api.nvim_set_hl(0, "@parameter.python", { link = "GruvboxYellowBold" })
-    end
-
-    api.nvim_set_hl(0, "Delimiter", { link = "GruvboxGray" })
-end
