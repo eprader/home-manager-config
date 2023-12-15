2d36fb7efe87c1c355e6bda406f4b9dee1563d73
eprader <56026248+eprader@users.noreply.github.com>
2023-12-16 00:38:48 +0100

    move starship to its own module

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@ in
 
   imports = [
     ./modules/kitty
+    ./modules/starship
     ./modules/lsd.nix
     ./modules/nvim
     ./modules/latex.nix
@@ -140,43 +141,6 @@ in
       Shortcuts = {
         TYPE_COPY = "Return";
       };
-    };
-  };
-
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = false;
-      format = ''
-        $username$hostname( $shlvl)( $directory)( $git_branch$git_commit$git_state$git_status)( $cmd_duration)( $character) '';
-      username = {
-        format = "[$user]($style)";
-        show_always = true;
-      };
-      hostname = {
-        format = "[@$hostname]($style)";
-        style = "bold green";
-        ssh_only = false;
-      };
-      shlvl = {
-        format = "[$shlvl]($style)";
-        style = "bold cyan";
-        threshold = 2;
-        repeat = true;
-      };
-      cmd_duration = {
-        format = "took [$duration]($style)";
-      };
-      directory = {
-        format = "[$path]($style)( [$read_only]($read_only_style))";
-        style = "bold blue";
-      };
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
-      };
-      scan_timeout = 10;
-      package.disabled = true;
     };
   };
 
