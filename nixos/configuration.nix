481682c34199d4aa4a1b3b777610dd89d20b764c
eprader <56026248+eprader@users.noreply.github.com>
2024-01-10 10:57:49 +0100

    redo virtualization formatting

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -77,11 +77,13 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  virtualisation.docker = {
-    enable = true;
-    rootless = {
+  virtualisation = {
+    docker = {
       enable = true;
-      setSocketVariable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
     };
   };
 
