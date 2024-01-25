d1a3eb537b18107cacd9c7b7a64cab887513f10a
eprader <56026248+eprader@users.noreply.github.com>
2024-01-25 22:07:43 +0100

    remove schedule for notify as it is not needed

--- modules/nvim/plugins/sentinel/lua/sentinel/init.lua
+++ modules/nvim/plugins/sentinel/lua/sentinel/init.lua
@@ -7,13 +7,11 @@ function M._proxy_require(modname)
     if not success then
         local ok, notifyer = pcall(M._real_require, "sentinel.notifyer")
         if not ok then
-            vim.schedule(function()
-                vim.notify(
-                    "Unable to load `sentinel.notifyer`",
-                    "error",
-                    { title = "Sentinel internal" }
-                )
-            end)
+            vim.notify(
+                "Unable to load `sentinel.notifyer`",
+                "error",
+                { title = "Sentinel internal" }
+            )
             return false
         end
         notifyer.notify(modname, module)
