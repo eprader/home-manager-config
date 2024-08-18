676baa2cd259e2d2622867740e49ec4b5032426f
eprader <56026248+eprader@users.noreply.github.com>
2024-08-18 05:25:52 +0200

    git: add deprecation note

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -4,6 +4,7 @@
     enable = true;
     userName = "eprader";
     userEmail = "56026248+eprader@users.noreply.github.com";
+    # INFO: Deprecated and will be removed
     # extraConfig = builtins.readFile ./.gitconfig;
     extraConfig = {
       pull = {
