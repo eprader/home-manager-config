261b8ee4f54f7c69239c1be91d7bce2396a7efa4
eprader <56026248+eprader@users.noreply.github.com>
2024-08-18 06:35:36 +0200

    hyprland(nix): remove `WAYLAND_DISPLAY` environment variable as it is already being exported

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -9,10 +9,6 @@
     # INFO: This option hints electron apps to use wayland
     NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland";
-    # INFO:
-    # As of 2024/09/18: The Discord launch script checks for both `QT_QPA_PLATFORM` and `WAYLAND_DISPLAY`.
-    # https://github.com/NixOS/nixpkgs/blob/5c82cc835ae9d7d7aed8e338d577c3241e60cdcd/pkgs/applications/networking/instant-messengers/discord/linux.nix#L108
-    WAYLAND_DISPLAY = "";
   };
 
   xdg = {
