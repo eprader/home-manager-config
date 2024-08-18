c74ab4d94037c13ddd26956a19f1fa1ff41fca44
eprader <56026248+eprader@users.noreply.github.com>
2024-08-18 05:24:44 +0200

    hyprland(wayland): Fix `NIXOS_ZONE_WL` and add `WAYLAND:DISPLAY` to fix `discord` fractional scaling

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -7,15 +7,19 @@
 
   environment.sessionVariables = {
     # INFO: This option hints electron apps to use wayland
-    NIXOS_OXOZONE_WL = "1";
+    NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland";
+    # INFO:
+    # As of 2024/09/18: The Discord launch script checks for both `QT_QPA_PLATFORM` and `WAYLAND_DISPLAY`.
+    # https://github.com/NixOS/nixpkgs/blob/5c82cc835ae9d7d7aed8e338d577c3241e60cdcd/pkgs/applications/networking/instant-messengers/discord/linux.nix#L108
+    WAYLAND_DISPLAY = "";
   };
 
   xdg = {
     portal = {
       enable = true;
       extraPortals = with pkgs; [
-        xdg-desktop-portal 
+        xdg-desktop-portal
         xdg-desktop-portal-gtk
       ];
     };
@@ -24,7 +28,7 @@
   hardware = {
     opengl.enable = true;
   };
-  
+
   services.xserver = {
     enable = true;
     displayManager.gdm = {
