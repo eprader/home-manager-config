5f219e2bc79874c2d9ce6612b8c9c9608105f665
eprader <56026248+eprader@users.noreply.github.com>
2025-03-18 14:48:57 +0100

    home-manager(hyprland): move to `nixos-unstable`

--- modules/hypr/default.nix
+++ modules/hypr/default.nix
@@ -1,16 +1,15 @@
 { pkgs, ... }:
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    package = unstable.hyprland;
     extraConfig = builtins.readFile ./hyprland.conf;
   };
 
   home.packages = with pkgs; [
     capitaine-cursors
   ];
-
-  # sessionVariables = {
-  #   VISUAL = "$EDITOR";
-  # };
 }
-
