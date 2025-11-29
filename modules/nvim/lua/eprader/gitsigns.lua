e82fc009ab22b7d49c409ae79ab69cb9a837883d
eprader <56026248+eprader@users.noreply.github.com>
2025-11-29 15:10:06 +0100

    nvim(gitsigns): remove yadm for now

--- modules/nvim/lua/eprader/gitsigns.lua
+++ modules/nvim/lua/eprader/gitsigns.lua
@@ -65,9 +65,9 @@ gitsigns.setup {
         row = 0,
         col = 1,
     },
-    yadm = {
-        enable = false,
-    },
+    -- yadm = {
+    --     enable = false,
+    -- },
 
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
