62ce1435fc86d7cdad4c1da860410c8381fd6f3b
eprader <56026248+eprader@users.noreply.github.com>
2026-01-31 00:04:54 +0100

    dunst: add initial config

--- modules/dunst/default.nix
+++ modules/dunst/default.nix
@@ -8,6 +8,24 @@
 
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        width = "(300, 600)";
+        height = "(0, 300)";
+
+        frame_width = 1;
+        frame_color = "#ebdbb2";
+        foreground = "#ebdbb2";
+        background = "#282828";
+        corner_radius = 5;
+        corners = "all";
+        font = "Fira Code Nerd Font Mono 12";
+        gap_size = 8;
+
+        notification_limit = 5;
+
+      };
+    };
   };
 }
 
