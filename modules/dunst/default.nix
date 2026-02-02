31b8d05b0a793fd4babeb11591380f449d230c6d
eprader <56026248+eprader@users.noreply.github.com>
2026-02-02 20:26:57 +0100

    dunst: configure urgency colours

--- modules/dunst/default.nix
+++ modules/dunst/default.nix
@@ -8,24 +8,63 @@
 
   services.dunst = {
     enable = true;
-    settings = {
-      global = {
-        width = "(300, 600)";
-        height = "(0, 300)";
+    settings =
+      {
+        global = {
+          width = "(300, 600)";
+          height = "(0, 300)";
+          offset = "(16, 16)";
 
-        frame_width = 1;
-        frame_color = "#ebdbb2";
-        foreground = "#ebdbb2";
-        background = "#282828";
-        corner_radius = 5;
-        corners = "all";
-        font = "Fira Code Nerd Font Mono 12";
-        gap_size = 8;
+          frame_width = 1;
+          frame_color = "#ebdbb2";
+          foreground = "#ebdbb2";
+          background = "#282828";
+          corner_radius = 5;
+          corners = "all";
+          font = "Fira Code Nerd Font Mono 12";
+          gap_size = 8;
 
-        notification_limit = 5;
+          notification_limit = 5;
 
+        };
+
+        # black = "#282828",
+        # dark_grey = "#928374",
+        #
+        # dark_red = "#cc241d",
+        # red = "#fb4934",
+        #
+        # dark_green = "#98971a",
+        # green = "#b8bb26",
+        #
+        # dark_yellow = "#d79921",
+        # yellow = "#fabd2f",
+        #
+        # dark_blue = "#458588",
+        # blue = "#83a598",
+        #
+        # dark_magenta = "#b16286",
+        # magenta = "#d3869b",
+        #
+        # dark_cyan = "#689d6a",
+        # cyan = "#8ec07c",
+        #
+        # light_grey = "#a89984",
+        # white = "#ebdbb2",
+        #
+        # dark_orange = "#d65d0e",
+        # orange = "#fe8019",
+
+        urgency_low = {
+          frame_color = "#83a598";
+        };
+        urgency_normal = {
+          frame_color = "#98971a";
+        };
+        urgency_critical = {
+          frame_color = "#cc241d";
+        };
       };
-    };
   };
 }
 
