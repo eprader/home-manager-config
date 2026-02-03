7fbbf7841b5872d2c3638660239592ed26ad6bb8
eprader <56026248+eprader@users.noreply.github.com>
2026-02-03 19:58:04 +0100

    nvim(harpoon): rename map to set

--- modules/nvim/lua/eprader/harpoon.lua
+++ modules/nvim/lua/eprader/harpoon.lua
@@ -37,17 +37,17 @@ harpoon.setup {
 -- KEYBINDS
 require "eprader.mapleader"
 
-local map = vim.keymap.set
+local set = vim.keymap.set
 local harpoon_mark = require "harpoon.mark"
 local harpoon_ui = require "harpoon.ui"
-map("n", "<leader>ha", harpoon_mark.add_file)
-map("n", "<leader>hl", harpoon_ui.toggle_quick_menu)
-map("n", "<C-j>", function()
+set("n", "<leader>ha", harpoon_mark.add_file)
+set("n", "<leader>hl", harpoon_ui.toggle_quick_menu)
+set("n", "<C-j>", function()
     harpoon_ui.nav_file(1)
 end)
-map("n", "<C-k>", function()
+set("n", "<C-k>", function()
     harpoon_ui.nav_file(2)
 end)
-map("n", "<C-l>", function()
+set("n", "<C-l>", function()
     harpoon_ui.nav_file(3)
 end)
