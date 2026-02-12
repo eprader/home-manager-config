8be10a1d3af326f85a43d1815552b759c421ae36
eprader <56026248+eprader@users.noreply.github.com>
2026-02-12 17:31:00 +0100

    nixos(thunar): add archive plugin and volman

--- nixos/modules/thunar.nix
+++ nixos/modules/thunar.nix
@@ -1,11 +1,20 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.thunar = {
     enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
   };
 
   services = {
     gvfs.enable = true; # Mount, trash, etc...
-    tumbler.enable = true; # Thumbnails for images
+    tumbler.enable = true; # Thumbnails
   };
+
+  environment.systemPackages = with pkgs; [
+    file-roller
+    libgsf # Thumbnails for .odf files
+  ];
 }
