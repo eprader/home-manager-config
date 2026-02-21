d746ea4ea7ad9518adcde2d40debd14466644ab7
eprader <56026248+eprader@users.noreply.github.com>
2026-02-21 16:02:23 +0100

    nix(clean): rename variable in `clean.sh` add `docker system prune` to clear up docker storage

--- clean.sh
+++ clean.sh
@@ -4,12 +4,13 @@ output=$(nix-env --list-generations)
 
 generation_ids=$(echo "$output" | awk '{print $1}' | grep -E '^[0-9]+$')
 
-all_but_last_three_generations=$(echo "$generation_ids" | sort -n | head -n -2 | tr '\n' ' ')
+all_but_last_two_generations=$(echo "$generation_ids" | sort -n | head -n -2 | tr '\n' ' ')
 
 # INFO:
-# Will result in a no-op if `all_but_last_three_generations` is empty.
-nix-env --delete-generations $all_but_last_three_generations
+# Will result in a no-op if `all_but_last_two_generations` is empty.
+nix-env --delete-generations $all_but_last_two_generations
 
 nix-store --gc
 
-echo "Size of /nix/store: $(du -sh /nix | awk '{print $1}')"
+# NOTE: For a large nix store this takes a while.
+# echo "Size of /nix/store: $(du -sh /nix | awk '{print $1}')"
