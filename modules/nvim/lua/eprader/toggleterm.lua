616f64a31a912b64493d6bec65b21c1c0398ffae
eprader <56026248+eprader@users.noreply.github.com>
2026-04-08 22:31:17 +0200

    nvim(toggleterm): change terminal height to be dynamic based on the number of lines of the origin window

--- modules/nvim/lua/eprader/toggleterm.lua
+++ modules/nvim/lua/eprader/toggleterm.lua
@@ -5,9 +5,9 @@ toggleterm.setup {
     -- size can be a number or function which is passed the current terminal
     size = function(term)
         if term.direction == "horizontal" then
-            return 15
+            return vim.o.lines * 0.5
         elseif term.direction == "vertical" then
-            return vim.o.columns * 0.4
+            return vim.o.columns * 0.5
         end
     end,
     open_mapping = [[<c-t>]],
