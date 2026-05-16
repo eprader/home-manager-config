645d887c730ad4d5ccc6ef469f181cf396328047
eprader <56026248+eprader@users.noreply.github.com>
2026-05-16 22:06:30 +0200

    nvim(treesitter): load treesitter for each filetype. still need way to install parsers

--- modules/nvim/lua/eprader/treesitter.lua
+++ modules/nvim/lua/eprader/treesitter.lua
@@ -6,3 +6,11 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- TODO: Figure out a way to install treesitter parsers without plugin
+-- NOTE: We need to manually start treesitter for each filetype
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "<filetype>" },
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
